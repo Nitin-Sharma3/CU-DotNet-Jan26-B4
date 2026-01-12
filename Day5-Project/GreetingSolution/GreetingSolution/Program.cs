2eeafb7cdb1d4b02ca22181a02f914b8dd742abf
NitinSharma3 <nitinsharma85804@gmail.com>
2026-01-12 12:44:22 +0530

    New Day 5 project upload

---
+++ Day5-Project/GreetingSolution/GreetingSolution/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace GreetingSolution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write your name : ");
+            string s = null;
+            s= Console.ReadLine();
+            Console.WriteLine(GreetingLibrary.GreetingHelper.GetGreeting(s));
+        }
+    }
+}
