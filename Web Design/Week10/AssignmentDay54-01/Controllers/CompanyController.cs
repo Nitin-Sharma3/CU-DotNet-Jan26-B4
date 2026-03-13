607fc61d01c77f5cd420c03d82d43bf6f4064108
NitinSharma3 <nitinsharma85804@gmail.com>
2026-03-13 10:58:50 +0530

    added day 54-01

---
+++ Web Design/Week10/AssignmentDay54-01/Controllers/CompanyController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheCorporatePulsePortal.Models;
+
+namespace TheCorporatePulsePortal.Controllers
+{
+    public class CompanyController : Controller
+    {
+        List<ItEmployee> emp = new List<ItEmployee>()
+        {
+            new ItEmployee(){ EmpId=1, Jobrole="Analyst", Name="Nitin", Salary=50000,active=true},
+            new ItEmployee(){ EmpId=2, Jobrole="Developer", Name="Ayush", Salary=40000,active=true},
+            new ItEmployee(){ EmpId=3, Jobrole="DRS Tracker", Name="Aditya", Salary=55000,active=true},
+            new ItEmployee(){ EmpId=4, Jobrole="CodeChef", Name="Aniket", Salary=60000,active=true},
+            new ItEmployee(){ EmpId=5, Jobrole="Programmer Analyst", Name="Lovely", Salary=80000,active=false}
+        };
+        string DailyAnnouncement = "The Aim for today is maximum efficiency. All the best!";
+        public IActionResult Index()
+        {
+            TempData["data"]=emp;
+            ViewBag.DailyAnnouncement = DailyAnnouncement;
+            return View();
+        }
+    }
+}
---
+++ Web Design/Week10/AssignmentDay54-01/Models/ItEmployee.cs
@@ -0,0 +1,11 @@
+﻿namespace TheCorporatePulsePortal.Models
+{
+    public class ItEmployee
+    {
+        public int EmpId { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public string Jobrole { get; set; }
+        public bool active { get; set; }
+    }
+}
