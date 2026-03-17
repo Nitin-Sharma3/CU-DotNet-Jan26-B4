e52bdbc5efa9cc7ec55028e8889abe90a72d9f49
NitinSharma3 <nitinsharma85804@gmail.com>
2026-03-17 12:28:07 +0530

    corrected

---
+++ Weekwise asessments(saturday)/Week10AsessmentUpdated/Models/Transaction.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace FinTrackPro.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public double Amount { get; set; }
+
+        public string Category { get; set; }
+
+        public DateTime Date { get; set; }
+
+        [ValidateNever]
+        [Display(Name ="Account Holder Name")]
+        public int AccountId { get; set; }
+        [ValidateNever]
+        public Account Account { get; set; }
+    }
+}
