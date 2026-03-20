2578c7f62819dca0cc0e2423538a2349e443b689
NitinSharma3 <nitinsharma85804@gmail.com>
2026-03-20 11:49:54 +0530

    added day 62-01

---
+++ Web Design/Week11/Day62-01/Data/Day62_01Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Day62_01.Models;
+
+namespace Day62_01.Data
+{
+    public class Day62_01Context : DbContext
+    {
+        public Day62_01Context (DbContextOptions<Day62_01Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Day62_01.Models.Loan> Loan { get; set; } = default!;
+    }
+}
---
+++ Web Design/Week11/Day62-01/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Day62_01.Data;
+
+namespace Day62_01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<Day62_01Context>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("Day62_01Context") ?? throw new InvalidOperationException("Connection string 'Day62_01Context' not found.")));
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
